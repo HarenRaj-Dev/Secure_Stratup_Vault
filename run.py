f9ef36a66dadbdec67d9a2354bbb1fb299c9c1d0
HarenRaj-Dev <koyalkarpulkalharenraj@gmail.com>
2026-01-13 16:29:54 +0530

    changes of render

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
+import os
 from vault import create_app
 
 # Create the app instance using the factory pattern
@@ -5,4 +6,5 @@ app = create_app()
 
 if __name__ == "__main__":
     # College projects usually run on port 5000
-    app.run(debug=True, port=5000)
+    port=int(os.environ.get("PORT",5000))
+    app.run(host="0.0.0.0",port=port)
